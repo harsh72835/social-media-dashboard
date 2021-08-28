cf6ba3a661a0edb1cd01c8b64cdce0d66a2d5f80
dharmesh-data <dp76070@gmail.com>
2021-08-28 22:05:25 +0530

    comiting files for final time

---
+++ index.js
@@ -0,0 +1,8 @@
+let toggle = document.querySelector("input[name=theme]");
+toggle.addEventListener("change", function () {
+  if (this.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+});
